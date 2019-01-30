a10089c6c69d1e36f4e06265ac93c086f6df483a
ElShpak <goohiko4@gmail.com>
2019-01-31 00:38:11 +0200

    deploy

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# остановить публикацию при ошибках
+set -e
+
+# сборка
+npm run build
+
+# переход в каталог сборки
+cd dist
+
+
+
+git init
+git add -A
+git commit -m 'deploy'
+
+
+
+
+ git push -f git@github.com:ElShpak/landingpage.git master:gh-pages
+
+cd -
